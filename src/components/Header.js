102fcc28de63788251c99f18fb83b0b4118a5c40
Mayank Kaushik <mayankkaushik187@gmail.com>
2023-09-09 02:33:44 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react"
+import AppBar from "@mui/material/AppBar"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+import IconButton from "@mui/material/IconButton"
+import logo from "../assets/logo.png"
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        className="logo"
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, fontSize: "3rem" }}
+                    >
+                        quizzer.
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
+
+export default Header
